d9b3af69022a1e29697e31d599644884a76b0a42
Ashish1208-create <at605075@gmail.com>
2026-05-30 19:58:20 +0530

    day 2 30/05/2026

---
+++ demo.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class demo {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+
+    for(int i=0; i<n; i++){
+      nums[i] = sc.nextInt();
+    }
+
+    int shift=0;
+
+    for(int i=1; i<n; i++){
+      int key = nums[i];
+      int j = i-1;
+      
+      while (j>=0 && nums[j]>key) {
+        nums[j+1] = nums[j];
+        shift++;
+        j--;
+      }
+      nums[j+1] = key;
+    }
+
+    for(int x : nums){
+      System.out.print(x  + " ");
+    }
+    System.out.println();
+    System.out.println(shift);
+
+    sc.close();
+  }
+}
