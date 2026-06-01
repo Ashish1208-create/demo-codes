dbe24ffade1dd3766302c651fba646186551dd75
Ashish1208-create <at605075@gmail.com>
2026-06-02 00:03:45 +0530

    1st june Q1

--- demo.java
+++ demo.java
@@ -2,38 +2,44 @@ import java.util.*;
 
 public class demo {
 
+  public static int countVowel(String str) {
+    int vowel = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+          || str.charAt(i) == 'u')
+        vowel++;
+    }
+    return vowel;
+  }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
     int n = sc.nextInt();
-    int[] nums = new int[n];
+    String[] names = new String[n];
 
     for (int i = 0; i < n; i++) {
-      nums[i] = sc.nextInt();
+      names[i] = sc.next();
     }
 
-    int swap = 0;
+    Arrays.sort(names, (a, b) -> {
+      int va = countVowel(a);
+      int vb = countVowel(b);
 
-    for (int i = 0; i < n - 1; i++) {
-      int miniIndex = i;
+      if (va != vb)
+        return vb - va;
 
-      for (int j = i + 1; j < n; j++) {
-        if (nums[j] < nums[miniIndex])
-          miniIndex = j;
-      }
+      if (a.length() != b.length())
+        return a.length() - b.length();
 
-      if (miniIndex != i) {
-        int temp = nums[i];
-        nums[i] = nums[miniIndex];
-        nums[miniIndex] = temp;
-        swap++;
-      }
+      return a.compareTo(b);
+    });
+
+    for (String name : names) {
+      System.out.println(name);
     }
 
-    for (int x : nums) {
-      System.out.print(x + " ");
-    }
-    System.out.println();
-    System.out.println(swap);
     sc.close();
   }
 }
