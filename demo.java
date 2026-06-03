e3738681cc7e2fab4678c12773d6185a607f6771
Ashish1208-create <at605075@gmail.com>
2026-06-03 14:17:08 +0530

    3rd June Q1

--- demo.java
+++ demo.java
@@ -4,33 +4,21 @@ public class demo {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    LinkedList<Integer> attLinkedList = new LinkedList<>();
 
-   int days = sc.nextInt();
-   int minDay = sc.nextInt();
-   int minHours = sc.nextInt();
-
-    int[] hours = new int[days];
-
-    for(int i=0; i<days; i++){
-      hours[i] = sc.nextInt();
-    }
-
-    int maxSum = 0;
-
-    for(int i=0; i<minDay; i++){
-      maxSum += hours[i];
+    for(int i=0; i<n; i++){
+      int temp = sc.nextInt();
+      attLinkedList.add(temp);
     }
     
-    int windowSum = maxSum;
+    System.out.println(attLinkedList);
 
-    for(int i=minDay; i<days; i++){
-      windowSum += hours[i] - hours[i-minDay]; 
-      maxSum = Math.max(maxSum, windowSum);
+    Collections.reverse(attLinkedList);
+    for(int x : attLinkedList){
+      System.out.print(x + " ");
     }
-
-    System.out.println(maxSum);
-    System.out.println(maxSum > minHours ? "YES" : "NO");
-
     sc.close();
+
   }
 }
