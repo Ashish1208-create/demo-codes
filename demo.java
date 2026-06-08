452afa4ca8ba3d2ea2843a1184168f678c798ee1
Ashish1208-create <at605075@gmail.com>
2026-06-08 23:12:52 +0530

    6th June Q2

--- demo.java
+++ demo.java
@@ -1,60 +1,19 @@
 import java.util.*;
 
-class Node {
-    int data;
-    Node prev, next;
-
-    Node(int data) {
-        this.data = data;
-    }
-}
-
 public class demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int k = sc.nextInt();
 
-        Node head = null, tail = null;
+        System.out.println(fibo(n));
+    }
 
-        for (int i = 0; i < n; i++) {
-            Node temp = new Node(sc.nextInt());
+    public static int fibo(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-            if (head == null) {
-                head = tail = temp;
-            } else {
-                tail.next = temp;
-                temp.prev = tail;
-                tail = temp;
-            }
-        }
-
-        k %= n;
-
-        if (k > 0) {
-
-            Node newTail = head;
-
-            for (int i = 1; i < n - k; i++) {
-                newTail = newTail.next;
-            }
-
-            Node newHead = newTail.next;
-
-            newTail.next = null;
-            newHead.prev = null;
-
-            tail.next = head;
-            head.prev = tail;
-
-            head = newHead;
-        }
-
-        Node curr = head;
-        while (curr != null) {
-            System.out.print(curr.data + " ");
-            curr = curr.next;
-        }
+        n = fibo(n-1) + fibo(n-2);
+        
+        return n;
     }
 }
