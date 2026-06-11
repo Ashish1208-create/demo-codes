a2f06dba1f507e4f6d2df757af3b8dbadd9c1fc7
Ashish1208-create <at605075@gmail.com>
2026-06-11 10:25:13 +0530

    10 June Q2

--- demo.java
+++ demo.java
@@ -3,32 +3,15 @@ import java.util.*;
 public class demo{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
-
     int n = sc.nextInt();
-    int w = sc.nextInt();
-
-    int[] weight = new int[n];
+    LinkedList<Integer> temp = new LinkedList<>();
 
     for(int i=0; i<n; i++){
-      weight[i] = sc.nextInt();
+        temp.add(sc.nextInt());
     }
 
-    int maxLength = 0;
-    int sum = 0;
+    int mid = n/2;
 
-    int start = 0;
-
-    for(int i=0; i<n; i++){
-      sum += weight[i];
-
-      while(sum>w){
-        sum -= weight[start];
-        start++;
-      }
-
-      maxLength = Math.max(maxLength, (i - start + 1));
-    }
-
-    System.out.println(maxLength);
+   System.out.println(temp.get(mid));
   }
 }
