8481ea1f14017b6b52a15c65351b1e9ef82b6678
Ashish1208-create <at605075@gmail.com>
2026-06-13 12:25:10 +0530

    13 June Q1

--- demo.java
+++ demo.java
@@ -4,31 +4,39 @@ public class demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] nums = new int[n];
+        int m = sc.nextInt();
+
+        int[][] matix = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        // int maxi = Arrays.stream(nums).max().getAsInt();
-        // int left = 0;
-        int right = n - 1;
-        int maxArea = 0;
-        int left = 0;
-
-        while (left < right) {
-            int min = Math.min(nums[left], nums[right]);
-            int currArea = min * (right - left);
-            maxArea = Math.max(maxArea, currArea);
-
-            if (nums[left] < nums[right]) {
-                left++;
-            } else {
-                right--;
+            for (int j = 0; j < m; j++) {
+                matix[i][j] = sc.nextInt();
             }
-
         }
 
-        System.out.println(maxArea);
+        int target = sc.nextInt();
+        sc.close();
+
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matix[row][col] == target) {
+                System.out.println("Found");
+                return;
+            } else if (matix[row][col] < target) {
+                low = mid + 1;
+            } else
+                high = mid - 1;
+        }
+
+        System.out.println("Not Found");
+        return;
+
+        
     }
 }
